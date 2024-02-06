ae63edb8098ac83e0f786ad01138eee4f4b998cb
fangxingzhong <446498091@qq.com>
2024-02-06 05:30:21 +0000

    update src/main/java/com/fang/GitTest.java.
    Signed-off-by: fangxingzhong <446498091@qq.com>

--- src/main/java/com/fang/GitTest.java
+++ src/main/java/com/fang/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello hotfix");
         System.out.println("master test");
-        System.out.println("hotfix test1");
+        System.out.println("hotfix test3");
     }
 }
